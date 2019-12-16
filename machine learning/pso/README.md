1f4087ef02c8ea7330318584eece285628f15ea9
grassflamingo <24762386+grassFlamingo@users.noreply.github.com>
2019-12-16 21:33:06 +0800

    pso begin

---
+++ machine learning/pso/README.md
@@ -0,0 +1,7 @@
+# PSO Particle swarm optimization
+
+> https://en.wikipedia.org
+
+Before you run, please make sure OpenGL is installed. 
+```sudo apt-get install freeglut3-dev```
+
---
+++ machine learning/pso/main.c
@@ -0,0 +1,30 @@
+
+#include <GL/glut.h>
+
+void init() {
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glMatrixMode(GL_PROJECTION);
+  glOrtho(-5, 5, -5, 5, 5, 15);
+  glMatrixMode(GL_MODELVIEW);
+  gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0);
+}
+
+void display() {
+  glClear(GL_COLOR_BUFFER_BIT);
+  glColor3f(1.0, 0, 0);
+  glutWireTeapot(3);
+  glFlush();
+  glFlush();
+}
+
+int main(int argc, char *argv[]) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+  glutInitWindowPosition(0, 0);
+  glutInitWindowSize(600, 400);
+  glutCreateWindow("OpenGL 3D View");
+  init();
+  glutDisplayFunc(display);
+  glutMainLoop();
+  return 0;
+}
